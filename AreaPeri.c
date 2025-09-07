5b54dafe4a14c51abdbb0fd3d16f3bfaf71828a5
Abhishek01s <69300019+Abhishek01s@users.noreply.github.com>
2025-09-07 13:05:09 +0530

    Create AreaPeri.c

---
+++ AreaPeri.c
@@ -0,0 +1,13 @@
+// Program to calculate the area and perimeter of a rectangle based on its length and width
+
+#include <stdio.h>
+
+int main(){
+  float len, wid;
+  printf("Enter length: ");
+  scanf("%f", &len);
+  printf("Enter width: ");
+  scanf("%f", &wid);
+  printf("Area is: %f, Perimeter is %f", len*wid, 2*(len+wid));
+  return 0;
+}
