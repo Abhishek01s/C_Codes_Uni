14cc8175092eec17b56ecdd3a8996743f3305404
Abhishek01s <69300019+Abhishek01s@users.noreply.github.com>
2025-09-07 13:15:05 +0530

    Create add2.c

---
+++ add2.c
@@ -0,0 +1,11 @@
+// Program to input two numbers from user and print the sum
+
+#include <stdio.h>
+
+int main(){
+int a, b;
+printf("Enter the numbers in the format, num1 num2 : ");
+scanf("%d %d", &a, &b);
+printf("Sum is %d\n", a+b);
+return 0;
+}
